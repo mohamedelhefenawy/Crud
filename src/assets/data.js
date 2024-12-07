cb011f744dfbae185f5988261f804c3e39c44d83
mohamedelhefenawy <melhefnawy718@gmail.com>
2024-12-07 21:29:36 +0200

    Update code with latest changes

--- src/assets/data.js
+++ src/assets/data.js
@@ -1,23 +1,2 @@
-export const product =[{
-    title: 'lenovo',
-    price : 1000, 
-    taxas :20,
-    ads : 10,
-    discount : 20, 
-    category : 'Phone'
-},
-{
-    title: 'samsung',
-    price : 1200, 
-    taxas :20,
-    ads : 10,
-    discount : 20, 
-    category : 'Phone'
-},
-{title: 'asus',
-    price : 1000, 
-    taxas :20,
-    ads : 10,
-    discount : 20, 
-    category : 'laptop'}
+export const product =[
 ]
